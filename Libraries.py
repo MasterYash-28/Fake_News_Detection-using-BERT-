d630e19b4f76cfdd1a824ad6685809370f2bc708
unknown <yashbedmutha37@gmail.com>
2025-06-07 03:08:46 +0530

    Libraries to be used

---
+++ Libraries.py
@@ -0,0 +1,10 @@
+# Libraries requried for project - Fake_news_detection
+ 
+import selenium
+from bs4 import BeautifulSoup
+import requests
+import time
+from tqdm import tqdm
+from newspaper import Article
+import praw
+import pandas as pd
